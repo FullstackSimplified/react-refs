2778cdfec1ccfe3db3ee9f478f4daf0ac9181d84
Prasoon <Prasoon@Prasoons-MacBook-Pro.local>
2021-09-11 01:36:16 +0530

    React useRef and forwardRed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect, useRef } from "react";
+import CustomInput from "./CustomInput";
 function App() {
+  const [input, setInput] = useState("");
+  const count = useRef(1);
+  const inputRef = useRef();
+  useEffect(() => {
+    //setCount((prevState) => prevState + 1);
+    count.current += 1;
+  });
+
+  const clickHandler = () => {
+    console.log(inputRef.current);
+    const value = inputRef.current.value;
+    if (!value) inputRef.current.focus();
+    else alert(value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CustomInput
+        ref={inputRef}
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={clickHandler}>Submit </button>
+
+      <hr />
+      <div>You have entered "{input}"</div>
+      <div>Rerendered {count.current}</div>
     </div>
   );
 }
---
+++ src/CustomInput.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomInput = React.forwardRef((props, ref) => {
+  return <input ref={ref} {...props} />;
+});
+
+export default CustomInput;
